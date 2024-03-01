54c64ab590f3d4200d4178b6f69b1ad00b58ff26
HLOEY <default_crt@yahoo.com>
2024-03-01 13:11:08 +0700

    add qualification, services, modal box services

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,3 +26,69 @@ function linkAction() {
 navLink.forEach((navItem) => {
   navItem.addEventListener("click", linkAction);
 });
+
+// Show/hide skill list
+
+const skillsList = document.getElementsByClassName("skills__content");
+const skillsHeader = document.querySelectorAll(".skills__header");
+
+skillsHeader.forEach((headerItem) => {
+  headerItem.addEventListener("click", () => {
+    const clickedHeader = headerItem.parentNode.className;
+
+    for (var i = 0; i < skillsList.length; i++) {
+      console.log((skillsList[i].className = "skills__content skills-close"));
+    }
+    if (clickedHeader == "skills__content skills-close") {
+      headerItem.parentNode.className = "skills__content skills-open";
+    }
+  });
+});
+
+// QUALIFICATION TABS
+const tabs = document.querySelectorAll("[data-target]");
+const tabsContent = document.querySelectorAll("[data-content]");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    // get the content element
+    contentTarget = document.querySelector(tab.dataset.target);
+    // toggle for content
+    tabsContent.forEach((content) => {
+      content.classList.remove("qualification__active");
+    });
+    contentTarget.classList.add("qualification__active");
+    // toggle for section
+    tabs.forEach((tab) => {
+      tab.classList.remove("qualification__active");
+    });
+    tab.classList.add("qualification__active");
+  });
+});
+
+// service modal box
+const modalView = document.querySelectorAll(".services__modal");
+const modalBtns = document.querySelectorAll(".services__button");
+const modalCloseBtns = document.querySelectorAll(".services__modal-close");
+
+const showModal = (btnIndex) => {
+  modalView[btnIndex].classList.add("service__modal-show");
+};
+const closeModal = () => {
+  modalView.forEach((modal) => {
+    modal.classList.remove("service__modal-show");
+  });
+};
+
+modalBtns.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    showModal(index);
+  });
+});
+modalCloseBtns.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    modalView.forEach((modalView) => {
+      modalView.classList.remove("service__modal-show");
+    });
+  });
+});
